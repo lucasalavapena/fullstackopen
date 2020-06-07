286732ede286c41a10be4f77ebe6e3bdb7f7097b
lucasalavapena <lucas.alavapena@gmail.com>
2020-06-07 17:18:47 +0100

    finished part 2.1 and 2.2

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+
 
 // const Hello = ({ name, age }) => {
 //   const bornYear = () => new Date().getFullYear() - age
@@ -125,19 +124,48 @@ import ReactDOM from 'react-dom'
 // }
 // ReactDOM.render(<App />, document.getElementById('root'))
 
-const App = (props) => {
-  const [value, setValue] = useState(10)
+// const App = (props) => {
+//   const [value, setValue] = useState(10)
 
-  return (
-    <div>
-      {value}
-      <button>reset to zero</button>
-      <button onClick={setValue(0)}>button</button>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       {value}
+//       <button>reset to zero</button>
+//       <button onClick={setValue(0)}>button</button>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <App />, 
+//   document.getElementById('root')
+// )
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
 ReactDOM.render(
-  <App />, 
+  <App notes={notes} />,
   document.getElementById('root')
 )
