9946e3bf0ac6cec29ff4ad7598b73f50efa2fc2b
lucasalavapena <lucas.alavapena@gmail.com>
2020-06-08 17:46:03 +0100

    finished part2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect  } from 'react'
+import axios from 'axios'
 
 
 const Filter  = ({inputValue,filterFunction}) => {
@@ -36,16 +37,19 @@ const Persons   = ({persons,currFilter}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '39-44-5323523', id:1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id:2  },
-    { name: 'Dan Abramov', number: '12-43-234345', id:3  },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id:4 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ currFilter, setFilter ] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  
   const addPerson = (event) => {
     event.preventDefault()
     const nameObject = {
