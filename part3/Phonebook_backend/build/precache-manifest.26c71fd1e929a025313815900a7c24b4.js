00edc5bc742a569749b2223618b7e36fd7cd9a7d
lucasalavapena <lucas.alavapena@gmail.com>
2020-07-16 16:10:34 +0100

    added frontend to backend

---
+++ part3/Phonebook_backend/build/precache-manifest.26c71fd1e929a025313815900a7c24b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552eb8f6c5e34c94ad1b4c6a889ad81d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae82c6f496f4bd507da",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "8b2a03ca040f8d7413be",
+    "url": "/static/js/2.94ca2cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ca2cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae82c6f496f4bd507da",
+    "url": "/static/js/main.abb8b308.chunk.js"
+  },
+  {
+    "revision": "6b189a97c36646ef317f",
+    "url": "/static/js/runtime-main.2c31142c.js"
+  }
+]);
---
+++ part3/Phonebook_backend/build/precache-manifest.94bec2d99aa660304b5a6365571b6389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8986021599666027c971cdcfb77d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "528e2a02829626d08264",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "8b2a03ca040f8d7413be",
+    "url": "/static/js/2.94ca2cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ca2cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528e2a02829626d08264",
+    "url": "/static/js/main.cc9ed727.chunk.js"
+  },
+  {
+    "revision": "6b189a97c36646ef317f",
+    "url": "/static/js/runtime-main.2c31142c.js"
+  }
+]);
